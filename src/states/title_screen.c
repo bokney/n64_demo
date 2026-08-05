a458a0b05166d4d30b7336db46693edb589c2941
bokney <toknyk@gmail.com>
2026-08-05 15:41:55 +0100

    ✨ add title screen state skeleton

---
+++ src/states/title_screen.c
@@ -0,0 +1,18 @@
+
+#include "title_screen.h"
+
+void title_screen_init(void) {
+
+}
+
+uint8_t title_screen_update(void) {
+    return 0;
+}
+
+void title_screen_draw(surface_t *disp) {
+
+}
+
+uint8_t title_screen_exit(void) {
+    return 0;
+}
---
+++ src/states/title_screen.h
@@ -0,0 +1,9 @@
+
+#include <stdint.h>
+
+#include <libdragon.h>
+
+void title_screen_init(void);
+uint8_t title_screen_update(void);
+void title_screen_draw(surface_t *disp);
+uint8_t title_screen_exit(void);
