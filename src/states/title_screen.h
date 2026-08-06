1dd9d05cacca90a432c130d60df05caf622ee4e5
bokney <toknyk@gmail.com>
2026-08-06 18:17:01 +0100

    ♻️ refactor title_screen: remove draw phase

--- src/states/title_screen.h
+++ src/states/title_screen.h
@@ -1,9 +1,10 @@
+#ifndef TITLE_SCREEN_H
+#define TITLE_SCREEN_H
 
 #include <stdint.h>
 
-#include <libdragon.h>
-
 void title_screen_init(void);
 uint8_t title_screen_update(void);
-void title_screen_draw(surface_t *disp);
 uint8_t title_screen_exit(void);
+
+#endif
