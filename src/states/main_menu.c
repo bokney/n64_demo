202193e4c7391a8637d672e7aeb5a78f8e345feb
bokney <toknyk@gmail.com>
2026-08-06 18:17:01 +0100

    ♻️ refactor main_menu: remove draw phase

--- src/states/main_menu.c
+++ src/states/main_menu.c
@@ -1,20 +1,12 @@
-
 #include "main_menu.h"
 
 void main_menu_init(void) {
-
 }
 
 uint8_t main_menu_update(void) {
-
     return 0;
 }
 
-void main_menu_draw(surface_t *disp) {
-
-}
-
 uint8_t main_menu_exit(void) {
-
     return 0;
 }
--- src/states/main_menu.h
+++ src/states/main_menu.h
@@ -1,9 +1,10 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
 
 #include <stdint.h>
 
-#include <libdragon.h>
-
 void main_menu_init(void);
 uint8_t main_menu_update(void);
-void main_menu_draw(surface_t *disp);
 uint8_t main_menu_exit(void);
+
+#endif
