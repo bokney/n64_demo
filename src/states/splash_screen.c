c500160cac2347950fc86f5ddbc2b51da4aa893c
bokney <toknyk@gmail.com>
2026-08-09 04:02:51 +0100

    ♻️ migrate splash_screen to Position component (float x,y,z)

--- src/states/splash_screen.c
+++ src/states/splash_screen.c
@@ -16,23 +16,23 @@ void splash_screen_init(void) {
     rdpq_text_register_font(1, custom_font);
 
     entity_t bg_entity = ecs_create_entity();
-    Transform bg_t = {0, 0};
+    Position bg_p = {0.0f, 0.0f, 0.0f};
     Sprite bg_s = {320, 240, RGBA32(195, 167, 225, 0)};
-    ecs_add_transform(bg_entity, bg_t);
+    ecs_add_position(bg_entity, bg_p);
     ecs_add_sprite(bg_entity, bg_s);
 
     box_entity = ecs_create_entity();
-    Transform box_t = {100, 140};
+    Position box_p = {100.0f, 140.0f, 0.0f};
     Sprite box_s = {50, 50, RGBA32(149, 105, 200, 0)};
     InputMover box_m = {2, ACTION_UP, ACTION_DOWN, ACTION_LEFT, ACTION_RIGHT};
-    ecs_add_transform(box_entity, box_t);
+    ecs_add_position(box_entity, box_p);
     ecs_add_sprite(box_entity, box_s);
     ecs_add_input_mover(box_entity, box_m);
 
     text_entity = ecs_create_entity();
-    Transform text_t = {100, 100};
+    Position text_p = {100.0f, 100.0f, 0.0f};
     Text text_d = {"SPLASH SCREEN ACTIVE", 1, RGBA32(255, 255, 255, 255)};
-    ecs_add_transform(text_entity, text_t);
+    ecs_add_position(text_entity, text_p);
     ecs_add_text(text_entity, text_d);
 }
 
