3872b41d60e8a336118e8c28dfa338246529d03c
bokney <toknyk@gmail.com>
2026-08-11 17:41:35 +0100

    ♻️ add game over state boilerplate

---
+++ src/states/game_over.c
@@ -0,0 +1,19 @@
+#include "game_over.h"
+
+void game_over_init(void) {
+
+}
+
+uint8_t game_over_update(void) {
+
+    return 0;
+}
+
+uint8_t game_over_exit(void) {
+
+    return 0;
+}
+
+void game_over_render_3d(T3DViewport *viewport) {
+    
+}
