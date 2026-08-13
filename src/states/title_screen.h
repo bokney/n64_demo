30e0f166595b4d46b8cc07771b3408fafb71157c
bokney <toknyk@gmail.com>
2026-08-13 18:39:51 +0100

    ✨ add title_screen_render declaration

--- src/states/title_screen.h
+++ src/states/title_screen.h
@@ -2,9 +2,11 @@
 #define TITLE_SCREEN_H
 
 #include <stdint.h>
+#include <t3d/t3d.h>
 
 void title_screen_init(void);
 uint8_t title_screen_update(void);
+void title_screen_render(T3DViewport *viewport);
 uint8_t title_screen_exit(void);
 
 #endif
