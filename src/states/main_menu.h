6449a6f3ece1a6f13b30178f45c5fa2fda9c80ad
bokney <toknyk@gmail.com>
2026-08-13 21:41:43 +0100

    ♻️ refactor main_menu to use ECS entities for cube rendering

--- src/states/main_menu.h
+++ src/states/main_menu.h
@@ -2,11 +2,9 @@
 #define MAIN_MENU_H
 
 #include <stdint.h>
-#include <t3d/t3d.h>
 
 void main_menu_init(void);
 uint8_t main_menu_update(void);
-void main_menu_render(T3DViewport *viewport);
 uint8_t main_menu_exit(void);
 
 #endif
