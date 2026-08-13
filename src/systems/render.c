6c72801ac305d55c0d4e490fee178fb2f0936438
bokney <toknyk@gmail.com>
2026-08-13 21:41:43 +0100

    ♻️ add ECS-driven render system with lighting integration

--- src/systems/render.c
+++ src/systems/render.c
@@ -1,20 +1,64 @@
 #include "render.h"
 #include "../state.h"
 #include "camera.h"
-#include "t3d/t3d.h"
+#include "lighting.h"
+#include "../ecs.h"
+#include <t3d/t3d.h>
 
 #define FB_COUNT 3
 
 static T3DViewport viewport;
 static bool viewport_initialized = false;
 
+static T3DMat4FP *model_mats = NULL;
+static int frame_idx = 0;
+
+void render_system_draw(T3DViewport *viewport) {
+    if (!model_mats) {
+        model_mats = malloc_uncached(sizeof(T3DMat4FP) * FB_COUNT);
+        frame_idx = 0;
+    }
+
+    frame_idx = (frame_idx + 1) % FB_COUNT;
+
+    for (entity_t e = 0; e < MAX_ENTITIES; e++) {
+        if (!entity_alive[e] || !has_mesh[e] || !has_position[e]) continue;
+
+        Position *pos = &positions[e];
+        float s[3] = {
+            has_scale[e] ? scales[e].x : 1.0f,
+            has_scale[e] ? scales[e].y : 1.0f,
+            has_scale[e] ? scales[e].z : 1.0f
+        };
+        float r[3] = {
+            has_rotation[e] ? rotations[e].pitch : 0.0f,
+            has_rotation[e] ? rotations[e].yaw   : 0.0f,
+            has_rotation[e] ? rotations[e].roll  : 0.0f
+        };
+        float t[3] = {pos->x, pos->y, pos->z};
+
+        t3d_mat4fp_from_srt_euler(&model_mats[frame_idx], s, r, t);
+        t3d_matrix_push(&model_mats[frame_idx]);
+        t3d_model_draw(meshes[e].model);
+        t3d_matrix_pop(1);
+    }
+}
+
 void render_tick(surface_t *disp, state *current) {
     (void)disp;
+    (void)current;
 
     if (!viewport_initialized) {
         viewport = t3d_viewport_create_buffered(FB_COUNT);
         viewport_initialized = true;
     }
 
+    t3d_frame_start();
     camera_system_apply(&viewport);
+
+    t3d_screen_clear_color(RGBA32(0x3f, 0x3f, 0x74, 0xff));
+    t3d_screen_clear_depth();
+
+    lighting_system_apply();
+    render_system_draw(&viewport);
 }
--- src/systems/render.h
+++ src/systems/render.h
@@ -2,8 +2,10 @@
 #define RENDER_H
 
 #include <libdragon.h>
+#include <t3d/t3d.h>
 #include "../state.h"
 
 void render_tick(surface_t *disp, state *current);
+void render_system_draw(T3DViewport *viewport);
 
 #endif
